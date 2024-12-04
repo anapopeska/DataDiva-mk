8d8b60ba35f65cad9175851ab88cfe8746c8e6af
anapopeska <166449873+anapopeska@users.noreply.github.com>
2024-12-04 23:20:51 +0100

    Add files via upload

---
+++ proekt/discord1/project1/src/main/java/com/example/project1/repository/CompanyHistoryPriceRepository.java
@@ -0,0 +1,17 @@
+package com.example.project1.repository;
+
+import com.example.project1.model.CompanyModel;
+import com.example.project1.model.CompanyHistoryPriceModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CompanyHistoryPriceRepository extends JpaRepository<CompanyHistoryPriceModel, Long> {
+    Optional<CompanyHistoryPriceModel> findByDateAndCompany(LocalDate date, CompanyModel company);
+    List<CompanyHistoryPriceModel> findByCompanyIdAndDateBetween(Long companyId, LocalDate from, LocalDate to);
+    List<CompanyHistoryPriceModel> findAllByDate(LocalDate date);
+}
